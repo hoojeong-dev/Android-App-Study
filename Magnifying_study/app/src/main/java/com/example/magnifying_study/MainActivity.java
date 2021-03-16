8fcf737a95d6f02415630ae49554328482a71ee0
gnwjd309 <fujeong15@naver.com>
2021-03-16 19:28:45 +0900

    Magnifying study

---
+++ Magnifying_study/app/src/main/java/com/example/magnifying_study/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.magnifying_study;
+
+import android.annotation.SuppressLint;
+import android.graphics.Rect;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Magnifier;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("ClickableViewAccessibility")
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CheckBox canShowOutside = findViewById(R.id.canShowOutside);
+        TextView sampleText = findViewById(R.id.sampleText);
+        Magnifier magnifier = new Magnifier(sampleText);
+        Rect outRect = new Rect();
+
+        int[] viewPosition = new int[2];
+
+        sampleText.post(new Runnable() {
+            @Override
+            public void run() {
+                sampleText.getDrawingRect(outRect);
+                sampleText.getLocationOnScreen(viewPosition);
+
+                outRect.offset(viewPosition[0], viewPosition[1]);
+            }
+        });
+
+        sampleText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                    case MotionEvent.ACTION_MOVE: {
+
+                        if (!canShowOutside.isChecked() &&
+                                !outRect.contains((int)event.getRawX(), (int)event.getRawX())
+                        ) {
+                            magnifier.dismiss();
+                            return false;
+                        }
+
+                        magnifier.show(event.getRawX() - viewPosition[0],
+                                event.getRawY() - viewPosition[1]);
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_UP: {
+                        magnifier.dismiss();
+                    }
+                }
+                return true;
+            }
+        });
+    }
+}
